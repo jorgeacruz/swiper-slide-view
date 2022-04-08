14893b33a6dab99aa958e6bf04c72f3d43168506
jorgeacruz <j.alexcruz@gmail.com>
2022-04-08 18:45:35 -0300

    Set timer slide

--- App.js
+++ App.js
@@ -3,14 +3,14 @@ import React from 'react';
 import { StyleSheet, Text, View, StatusBar, Dimensions, Image } from 'react-native';
 import Swiper from 'react-native-swiper';
 
-const {width, height} = Dimensions.get("screen");
+const {width, height} = Dimensions.get("window");
 
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar hidden={true} />
-        <Swiper autoplay={true}>
+        <Swiper autoplay={true} showsVerticalScrollIndicator={false}>
           <Image source={require('./src/images/player1.jpeg')} style={styles.ImageBG} />
           <Image source={require('./src/images/player2.jpeg')} style={styles.ImageBG} />
           <Image source={require('./src/images/player3.jpeg')} style={styles.ImageBG} />
@@ -18,6 +18,9 @@ export default function App() {
           <Image source={require('./src/images/player5.jpeg')} style={styles.ImageBG} />
           <Image source={require('./src/images/player6.jpeg')} style={styles.ImageBG} />
         </Swiper>
+        <View>
+          
+        </View>
       
     </View>
   );
